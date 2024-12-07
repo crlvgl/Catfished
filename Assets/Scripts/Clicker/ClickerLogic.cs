47f7efa384661a1a7e2bf46dff49cfe32da40723
Pepe Krumminga <krumminga.pepe@gmail.com>
2024-12-07 23:04:54 +0100

    Added a countdown for the minigame

--- Assets/Scripts/Clicker/ClickerLogic.cs
+++ Assets/Scripts/Clicker/ClickerLogic.cs
@@ -2,17 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ClickerLogic : MonoBehaviour {
+
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
     public event EventHandler<OnSpacePressedEventArgs> OnSpacePressed;
     public class OnSpacePressedEventArgs : EventArgs {
         public int spaceCount;
     }
 
-    public event ClickerLogicDelegate OnFloatEvent;
     public delegate void ClickerLogicDelegate(float f);
 
-    public event Action<bool, int> OnActionEvent;
     private int spaceCount;
     private void Start() {
 
@@ -20,14 +23,23 @@ public class ClickerLogic : MonoBehaviour {
 
 
     private void Update() {
+        
+        if (remainingTime > 0) {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0) {
+            remainingTime = 0;
+            // GameOver();
+            timerText.color = Color.red;
+        }
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
         if (Input.GetKeyDown(KeyCode.Space)) {
             // Space pressed!
             spaceCount++;
             OnSpacePressed?.Invoke(this, new OnSpacePressedEventArgs { spaceCount = spaceCount });
-
-            OnFloatEvent?.Invoke(5.5f);
-
-            OnActionEvent?.Invoke(true, 56);
         }
     }
 }
--- Assets/Scripts/Clicker/UpdateFish.cs
+++ Assets/Scripts/Clicker/UpdateFish.cs
@@ -12,25 +12,11 @@ public class UpdateFish : MonoBehaviour
     private void Start() {
         ClickerLogic clickerLogic = GetComponent<ClickerLogic>();
         clickerLogic.OnSpacePressed += ClickerLogic_OnSpacePressed;
-        clickerLogic.OnFloatEvent += ClickerLogic_OnFloatEvent;
-        clickerLogic.OnActionEvent += ClickerLogic_OnActionEvent;
-    }
-
-    private void ClickerLogic_OnActionEvent(bool arg1, int arg2) {
-        Debug.Log(arg1 + " " + arg2);
-    }
-
-    private void ClickerLogic_OnFloatEvent(float f) {
-        Debug.Log("float: " + f);
     }
 
     private void ClickerLogic_OnSpacePressed(object sender, ClickerLogic.OnSpacePressedEventArgs e) {
         Debug.Log("Space! " + e.spaceCount);
+        fishText.text = "Pets: " + e.spaceCount;
     }
 
-    public void AddFish()
-    {
-        pets++;
-        fishText.text = "Pets: " + pets;
-    }
 }
