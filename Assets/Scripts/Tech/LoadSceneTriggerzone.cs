dd4f566a417bacac008dd3f29755e6229be33f73
Carl Vogel <vogel.carl.l@gmail.com>
2024-12-11 22:25:31 +0100

    changed LoadingScreen Triggers, added Scenes [Crossroads, Sea, MushroomCave], imported MainCharacter anims

--- Assets/Scripts/Tech/LoadSceneTriggerzone.cs
+++ Assets/Scripts/Tech/LoadSceneTriggerzone.cs
@@ -9,7 +9,10 @@ public class LoadSceneTriggerzone : MonoBehaviour
     [Tooltip("The path to the loading screen scene. DO NOT TOUCH UNLESS YOU KNOW WHAT YOU'RE DOING")]
     public string loadingScreenPath = "Assets/Scenes/LoadingScreen.unity";
     [Tooltip("The time to wait before loading the scene. If set to 0, the scene will load immediately")]
+    public KeyCode pressedKey = KeyCode.Space;
     public float waitTime = 0.2f;
+
+    private bool canLoad = false;
 
     void Start()
     {
@@ -18,15 +21,37 @@ public class LoadSceneTriggerzone : MonoBehaviour
 
     void Update()
     {
-
+        if (Input.GetKeyDown(pressedKey))
+        {
+            // Debug.Log("Button has been pressed");
+            canLoad = true;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        // TODO
+        // add some pop-up text to show the player that they can press a key to load the scene
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
         if (other.gameObject.name == "Player")
         {
-            StartCoroutine(LoadScene());
+            // Debug.Log("Player has entered the trigger zone");
+            if (canLoad)
+            {
+                canLoad = false;
+                Debug.Log("Loading scene: " + scenePath);
+                StartCoroutine(LoadScene());
+            }
         }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // TODO
+        // remove the pop-up text
     }
 
     IEnumerator LoadScene()
