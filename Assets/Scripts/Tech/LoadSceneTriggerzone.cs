c1cee435c3c5fd75229a3a849aa234b9311186ef
Carl Vogel <vogel.carl.l@gmail.com>
2024-12-12 01:23:57 +0100

    modified LoadingZone script with new features

--- Assets/Scripts/Tech/LoadSceneTriggerzone.cs
+++ Assets/Scripts/Tech/LoadSceneTriggerzone.cs
@@ -12,11 +12,25 @@ public class LoadSceneTriggerzone : MonoBehaviour
     public KeyCode pressedKey = KeyCode.Space;
     public float waitTime = 0.2f;
 
+    public bool moveCharacter = false;
+    public bool moveCharacterRight = false;
+    private bool moveCharacterNow = false;
+    public bool fadeToBlack = false;
+    private bool fadeToBlackNow = false;
+    public float timeBeforeFade = 0.5f;
+    public SpriteRenderer blackScreen;
+    public float fadeSpeed = 1.0f;
+
+    public GameObject player;
+
     private bool canLoad = false;
 
     void Start()
     {
-
+        if (player == null)
+        {
+            player = GameObject.Find("Player");
+        }
     }
 
     void Update()
@@ -25,6 +39,23 @@ public class LoadSceneTriggerzone : MonoBehaviour
         {
             // Debug.Log("Button has been pressed");
             canLoad = true;
+        }
+
+        if (moveCharacterNow)
+        {
+            if (moveCharacterRight)
+            {
+                // player.transform.position = Vector3.MoveTowards(player.transform.position, new Vector3(player.transform.position.x + 20, player.transform.position.y, player.transform.position.z), player.GetComponent<PlayerMovement>().speed * Time.deltaTime);
+            }
+            else
+            {
+                player.transform.position = Vector3.MoveTowards(player.transform.position, new Vector3(player.transform.position.x - 20, player.transform.position.y, player.transform.position.z), player.GetComponent<PlayerMovement>().speed * Time.deltaTime);
+            }
+        }
+
+        if (fadeToBlackNow && blackScreen.color.a < 1)
+        {
+            blackScreen.color = new Color(0.0f, 0.0f, 0.0f, blackScreen.color.a + Time.deltaTime / fadeSpeed);
         }
     }
 
@@ -56,6 +87,17 @@ public class LoadSceneTriggerzone : MonoBehaviour
 
     IEnumerator LoadScene()
     {
+        if (moveCharacter)
+        {
+            Destroy(player.GetComponent<Rigidbody2D>());
+            moveCharacterNow = true;
+        }
+        if (fadeToBlack)
+        {
+            yield return new WaitForSeconds(timeBeforeFade);
+            fadeToBlackNow = true;
+        }
+        
         staticBackbone.sceneToLoad = scenePath;
 
         yield return new WaitForSeconds(waitTime);
