0fa102b7eb062a686d8a2ffe89940d578497e8bc
Carl Vogel <vogel.carl.l@gmail.com>
2024-12-19 02:53:40 +0100

    updated Titlescreen; adjusted CloudMovement on FieldTest

--- Assets/Scripts/UI/StartScreen/DestroyBubbles.cs
+++ Assets/Scripts/UI/StartScreen/DestroyBubbles.cs
@@ -4,8 +4,6 @@ public class DestroyBubbles : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log(other.gameObject.tag);
-
         if (other.gameObject.tag == "Bubble")
         {
             Destroy(other.gameObject);
